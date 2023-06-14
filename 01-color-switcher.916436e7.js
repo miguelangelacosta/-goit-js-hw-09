3ee24517aa38c7f3176f324b4a5a484feea48f4c
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-06-14 00:33:49 +0000

    Deploying to gh-pages from @ miguelangelacosta/-goit-js-hw-09@7f743ecd001d5a548eb36fe902f34ccad2e83e46 🚀

---
+++ 01-color-switcher.916436e7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.body;let a;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;d.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.916436e7.js.map
